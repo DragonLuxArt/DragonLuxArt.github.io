917ad5a7450a5236135e38ad96e00c09627128b4
Bartosz Kwiczala <bartek.kwiczala@gmail.com>
2017-07-06 19:56:53 +0200

    Change6

---
+++ Content/mainPageJS.js
@@ -0,0 +1,165 @@
+﻿$(".menuBar").hide();
+
+$(".mainPageTitle").hide();
+$(".mainPageUnderTitleText").hide();
+
+
+var mq = window.matchMedia("(max-width: 1365px)");
+if (mq.matches) {
+
+    $(".mainPageTitle").animate({
+        bottom: '50%'
+    }, {
+        duration: 4000,
+        queue: false
+    });
+
+    $(".mainPageTitle").fadeIn(5000, function () {
+        queue: false
+    });
+
+
+    $(".mainPageUnderTitleText").animate({
+        bottom: '35%'
+    }, {
+        duration: 3000,
+        queue: false
+    });
+
+    $(".mainPageUnderTitleText").fadeIn(5000, function () {
+        queue: false
+    });
+
+    $(".mainPageTitle").hover(function () {
+        $(".mainPageTitle").animate({
+            fontSize: '45px',
+
+        });
+    });
+
+    $(".mainPageTitle").mouseleave(function () {
+        $(".mainPageTitle").animate({
+            fontSize: '55px',
+        });
+    });
+
+    $(".mainPageUnderTitleText").hover(function () {
+        $(".mainPageUnderTitleText").animate({
+            fontSize: '30px'
+        });
+    });
+
+    $(".mainPageUnderTitleText").mouseleave(function () {
+        $(".mainPageUnderTitleText").animate({
+            fontSize: '40px',
+        });
+    });
+
+    $(".menuLine").hover(function () {
+        $(".menuLine").fadeOut("slow", function () {
+            $(".menuBar").slideDown("slow");
+        });
+    });
+
+
+    $(".menuBar").mouseleave(function () {
+        $(".menuBar").fadeOut("slow", function () {
+            $(".menuLine").fadeIn("slow");
+        });
+    });
+}
+else {
+    $(".mainPageTitle").animate({
+        bottom: '50%'
+    }, {
+        duration: 4000,
+        queue: false
+    });
+
+    $(".mainPageTitle").fadeIn(5000, function () {
+        queue: false
+    });
+
+
+    $(".mainPageUnderTitleText").animate({
+        bottom: '35%'
+    }, {
+        duration: 3000,
+        queue: false
+    });
+
+    $(".mainPageUnderTitleText").fadeIn(5000, function () {
+        queue: false
+    });
+
+    $(".mainPageTitle").hover(function () {
+        $(".mainPageTitle").animate({
+            fontSize: '60px',
+
+        });
+    });
+
+    $(".mainPageTitle").mouseleave(function () {
+        $(".mainPageTitle").animate({
+            fontSize: '70px',
+        });
+    });
+
+    $(".mainPageUnderTitleText").hover(function () {
+        $(".mainPageUnderTitleText").animate({
+            fontSize: '40px'
+        });
+    });
+
+    $(".mainPageUnderTitleText").mouseleave(function () {
+        $(".mainPageUnderTitleText").animate({
+            fontSize: '50px',
+        });
+    });
+
+
+    $(".menuLine").hover(function () {
+        $(".menuLine").fadeOut("slow", function () {
+            $(".menuBar").slideDown("slow");
+        });
+    });
+
+
+    $(".menuBar").mouseleave(function () {
+        $(".menuBar").fadeOut("slow", function () {
+            $(".menuLine").fadeIn("slow");
+        });
+    });
+
+}
+
+
+var mq = window.matchMedia("(max-width: 480px)");
+if (mq.matches) {
+    $(".mainPageTitle").animate({
+        bottom: '70%'
+    }, {
+        duration: 4000,
+        queue: false
+    });
+
+    $(".mainPageTitle").fadeIn(5000, function () {
+        queue: false
+    });
+
+
+    $(".mainPageUnderTitleText").animate({
+        bottom: '55%'
+    }, {
+        duration: 3000,
+        queue: false
+    });
+
+    $(".mainPageUnderTitleText").fadeIn(5000, function () {
+        queue: false
+    });
+
+    $(".menuLine").hide();
+    $(".menuBar").show();
+
+}
