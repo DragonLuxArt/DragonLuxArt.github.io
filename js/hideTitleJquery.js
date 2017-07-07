13a5cdb27d58cd072e8d6884837872592d71f9e0
Bartosz Kwiczala <bartek.kwiczala@gmail.com>
2017-07-07 19:09:32 +0200

    NewWebChange

---
+++ js/hideTitleJquery.js
@@ -0,0 +1,15 @@
+﻿$(".menu2").click(function () {
+    $(".UpperTitle").fadeOut("slow");
+
+});
+
+$(".menu3").click(function () {
+    $(".UpperTitle").fadeOut("slow");
+
+});
+
+
+$(".active").click(function () {
+    $(".UpperTitle").fadeIn("slow");
+
+});
