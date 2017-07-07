14a7dc1ca25cbabf59ab983b132df5ec438006c4
Bartosz Kwiczala <bartek.kwiczala@gmail.com>
2017-07-07 20:53:19 +0200

    NewWebDesign

---
+++ js/background.cycle-custom.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+
+var fadeShow = $(".background").fadeShow({
+
+
+
+	correctRatio: true,
+
+
+
+	shuffle: true,
+
+
+
+	speed: 4000,
+
+
+
+	images: ['images/backgroundimageSlider1.jpg',
+
+
+
+			 'images/slider-image-2.jpg']
+
+
+
+
+
+
+
+});
